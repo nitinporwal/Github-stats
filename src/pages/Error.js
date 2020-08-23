d11b891aff5928e503c7db2b0f9f9ea59259bedc
nitin-codr <nitinporwal99@gmail.com>
2020-08-23 18:58:28 +0530

    complete error page with inline styling

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Error extends Component {
     render() {
         return (
-            <div>
-                Error Page
+            <div style={{ "minHeight": "100vh", "display": "grid", "placeItems": "center", "textAlign": "center" }}>
+                <div>
+                    <h1 style={{ "fontSize": "10rem", "margin": "0" }}>404</h1>
+                    <h3 style={{ "fontSize": "1.5rem", "textTransform": "capitalize", "margin": "0" }}>sorry, the page you tried cannot be found</h3>
+                </div>
+                <Link to="/" style={{"marginTop": "-20%", "fontSize": "1.3rem", "textTransform": "uppercase", "display": "block", "padding": "0.5%", "background": "#03a890", "color": "white", "textDecoration": "none", "borderRadius": "4px" }}>
+                    back home
+                </Link>
             </div>
         )
     }
