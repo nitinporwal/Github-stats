08300dc524f0595f22098eb4d0636c70f7e8b8e7
nitin-codr <nitinporwal99@gmail.com>
2020-08-23 22:09:03 +0530

    add login page content

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -9,7 +9,7 @@ class Error extends Component {
                     <h1 style={{ "fontSize": "10rem", "margin": "0" }}>404</h1>
                     <h3 style={{ "fontSize": "1.5rem", "textTransform": "capitalize", "margin": "0" }}>sorry, the page you tried cannot be found</h3>
                 </div>
-                <Link to="/" style={{"marginTop": "-20%", "fontSize": "1.3rem", "textTransform": "uppercase", "display": "block", "padding": "0.5%", "background": "#03a890", "color": "white", "textDecoration": "none", "borderRadius": "4px" }}>
+                <Link to="/" style={{ "marginTop": "-20%", "border": "2px solid transparent", "boxShadow": "0 1px 3px rgba(0, 0, 0, 0.2)", "fontSize": "1.2rem", "fontWeight": "400", "textTransform": "uppercase", "display": "inline", "padding": "0.5%", "background": "#03a890", "color": "white", "textDecoration": "none", "borderRadius": "4px" }}>
                     back home
                 </Link>
             </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import loginImg from '../images/login-img.svg'
 
 class Login extends Component {
     render() {
         return (
-            <div>
-                From Login
+            <div style={{ "minHeight": "100vh", "display": "grid", "placeItems": "center", "width": "90vw", "maxWidth": "600px", "textAlign": "center", "margin": "auto" }}>
+                <img src={loginImg} alt="github user" style={{ "maxWidth": "100%" }} />
+                <h1 style={{ "textTransform": "capitalize", "margin": "0" }}>github user</h1>
+                <button style={{ "marginTop": "-20%", "border": "2px solid transparent", "boxShadow": "0 1px 3px rgba(0, 0, 0, 0.2)", "fontSize": "1.2rem", "fontWeight": "400", "textTransform": "uppercase", "display": "inline", "padding": "0.5%", "background": "#03a890", "color": "white", "textDecoration": "none", "borderRadius": "4px" }}>
+                    login
+                </button>
             </div>
         )
     }
