ff6c4d36e4e0605dbc4e14695d3a75b8f5d4d6d8
nitin-codr <nitinporwal99@gmail.com>
2020-08-23 17:20:04 +0530

    setup routing for pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Dashboard, Login, Error } from './pages';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Dashboard />
-      <Login />
-      <Error />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Dashboard />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
